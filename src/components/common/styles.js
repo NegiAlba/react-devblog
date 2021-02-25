03241e295e9a1b161db2aeefb8bbe30833f2b44c
Axel Gromat <axel.gromat@gmail.com>
2021-02-25 15:59:47 +0100

    Connecting to local API

--- src/components/common/styles.js
+++ src/components/common/styles.js
@@ -1,11 +1,12 @@
 
 const categoryColors = {
-    Tech: 'rgb(255,59,48)',
-    Test: 'rgb(0,113,164)',
-    Vue: 'rgb(52,199,89)',
-    React: 'rgb(64,156,255)',
-    JavaScript: 'rgb(255, 179, 64)',
-    Cloud: 'rgb(175,82,250)'
+    tech: 'rgb(255,59,48)',
+    test: 'rgb(0,113,164)',
+    vue: 'rgb(52,199,89)',
+    react: 'rgb(64,156,255)',
+    javascript: 'rgb(255, 179, 64)',
+    cloud: 'rgb(175,82,250)',
+    post: 'rgb(222,151,164)',
 }
 
 export {
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,16 +1,33 @@
-import Form from "../components/Form"
+import React, { useState, useEffect } from 'react'
 import trending from '../assets/mocks/trending';
 import {PostMasonry} from '../components/common';
+import axios from 'axios';
+
+
+const apiUrl = 'http://localhost:3000/api/v1/posts'
 
 const Blog = () => {
-
+    const [apiPosts,setApiPosts] = useState([]);
+    
+    async function getPosts() {
+        try {
+          const res = await axios.get(apiUrl);
+          console.log(res.data);
+          setApiPosts(res.data);
+        } catch (err) {
+          console.error(err);
+        }
+    }
+    useEffect(()=>{
+        getPosts()
+      },[])
     return (
         <section className="container">
             <div className="row">
-                <h2>Trending Posts</h2>
-                <PostMasonry posts={trending} columns={3}/>
+                <h2 className="blog-title">Trending Posts</h2>
+                <PostMasonry posts={trending} columns={4}/>
+                <PostMasonry posts={apiPosts} columns={3}/>
             </div>
-            <Form/>
         </section>
     )
 }
